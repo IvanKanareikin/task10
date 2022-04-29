686d274003e3b0297d6647c21623e2edf8aa0cb3
Ivan Kanareikin <kanareikin.iwan2017@yandex.ru>
2022-04-29 20:58:46 +0300

    Добавьте файлы проекта.

---
+++ task10.cpp
@@ -0,0 +1,10 @@
+﻿#include "Header.h"
+
+int main()
+{
+    Student Vanya("Kanareykin", "Ivan", "Leonidovich", 18, true);
+    Boss karpov("Karpov", "Dmitrii", "Anatolievich", 46, 28);
+    Vanya.print();
+    cout << endl;
+    karpov.print();
+}
